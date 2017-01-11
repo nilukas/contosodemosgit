4c22056e2853465c95aa8092265b6da944bcbf98
niluka <tharanga.subasinghe@gmail.com>
2017-01-11 11:07:26 +0530

    initial commit

---
+++ ContosoUnitTest/SampleUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ContosoUnitTest
+{
+    [TestClass]
+    public class SampleUnitTest
+    {
+        [TestMethod]
+        public void TestPass()
+        {
+            Assert.AreEqual(10, 10);
+        }
+        [TestMethod]
+        public void TestFail()
+        {
+            Assert.AreEqual(5, 5);
+        }
+    }
+}
